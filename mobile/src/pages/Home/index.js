8fe671848f221a3f36fc64b302853431769b8cfb
William Robert <willtaavares@gmail.com>
2020-04-21 20:30:21 -0300

    statusbar visible

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,108 +1,112 @@
-import React, {useState, useEffect} from 'react';
-import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
-import { View, FlatList, Text, Image, TouchableOpacity, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import {
+  View,
+  FlatList,
+  Text,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+  StatusBar,
+} from "react-native";
 
-import styles from './styles';
-import logoImg from '../../assets/logo.png'
+import styles from "./styles";
+import logoImg from "../../assets/logo.png";
 
-export default function Home(){
-    const [states, setStates] = useState([]);
-    const [total, setTotal] = useState(0);
-    const navigation = useNavigation();
-    const [loading, setLoading] = useState(true);
-    
-    function reload(){
-        setLoading(true)
-    }
-    async function requestBrasilCases(){
-        const totalCasesApi = 'https://covid19-brazil-api.now.sh/api/report/v1/brazil'
-        fetch(totalCasesApi)
-        .then(res =>{
-            return res.json()
-        })
-        .then(resBody =>{
-            setTotal(resBody.data.confirmed)
-        })
-    
-    }
-    async function request(){
-        const api = 'https://covid19-brazil-api.now.sh/api/report/v1'
-        await fetch(api)
-        .then(response =>{
-            return response.json();
-        })
-        .then(jsonBody =>{
-            const sortedData = jsonBody.data.sort((a,b) => (a.uf > b.uf ? 1 : -1));
+export default function Home() {
+  const [states, setStates] = useState([]);
+  const [total, setTotal] = useState(0);
+  const navigation = useNavigation();
+  const [loading, setLoading] = useState(true);
 
-            setStates([...sortedData])
-            
-        })
-        setLoading(false)
-    }
+  function reload() {
+    setLoading(true);
+  }
+  async function requestBrasilCases() {
+    const totalCasesApi =
+      "https://covid19-brazil-api.now.sh/api/report/v1/brazil";
+    fetch(totalCasesApi)
+      .then((res) => {
+        return res.json();
+      })
+      .then((resBody) => {
+        setTotal(resBody.data.confirmed);
+      });
+  }
+  async function request() {
+    const api = "https://covid19-brazil-api.now.sh/api/report/v1";
+    await fetch(api)
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonBody) => {
+        const sortedData = jsonBody.data.sort((a, b) => (a.uf > b.uf ? 1 : -1));
 
-    function navigateToDetail(state){
-        navigation.navigate('Detail', {state})
-    }
+        setStates([...sortedData]);
+      });
+    setLoading(false);
+  }
 
-    useEffect(()=>{
-        request();
-        requestBrasilCases();
-    }, [loading === true]);
+  function navigateToDetail(state) {
+    navigation.navigate("Detail", { state });
+  }
 
-    if(loading){
-        return(
-            <View style={styles.containerLoading}>
-                <ActivityIndicator 
-                size="large" 
-                color="#f76981"/>
-                <Text>
-                    Buscando casos ...
-                </Text>
-            </View>
-        )
-    } else
-    {
-        return(
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <Image source={logoImg} />
-                    <Text style={styles.headerText}>Total de <Text 
-                    style={styles.headerTextBold}>{total}</Text> casos</Text>
-                </View>
-                    <TouchableOpacity style={styles.reload} 
-                    onPress={() => reload()}>
-                        <Text style={styles.reloadText}>Atualizar</Text>                  
-                        <Feather name="refresh-cw" size={22} color="#f76981"/>
-                    </TouchableOpacity>
-                <Text style={styles.title}>Casos de Covid-19 no Brasil</Text>
-                <Text style={styles.description}>Comece escolhendo o estado</Text>
+  useEffect(() => {
+    request();
+    requestBrasilCases();
+  }, [loading === true]);
 
-                <FlatList
-                    data={states}
-                    style={styles.statesList}
-                    keyExtractor={state => `${state.uid}`}
-                    showsVerticalScrollIndicator={false}
-                    renderItem={({item: state})=>(
-                        <View style={styles.states}>
-                            <Text style={styles.stateProperty}>
-                            {state.state}/{state.uf}</Text>
-                            
-
-                            <Text style={styles.stateProperty}>Casos:</Text>
-                            <Text style={styles.stateValue}>{state.cases}</Text>
-
-                            <TouchableOpacity style={styles.detailsButton} 
-                            onPress={() => navigateToDetail(state)}>
-                                <Text style={styles.detailsButtonText}>
-                                Ver mais detalhes</Text>
-                                <Feather name="arrow-right" size={16} color="#f76981"/>
-                                    
-                            </TouchableOpacity>
-                        </View>
-                    )}
-                    />
-            </View>
+  if (loading) {
+    return (
+      <View style={styles.containerLoading}>
+        <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+        <ActivityIndicator size="large" color="#f76981" />
+        <Text>Buscando casos ...</Text>
+      </View>
     );
-}
+  } else {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+        <View style={styles.header}>
+          <Image source={logoImg} />
+          <Text style={styles.headerText}>
+            Total de <Text style={styles.headerTextBold}>{total}</Text> casos
+          </Text>
+        </View>
+        <TouchableOpacity style={styles.reload} onPress={() => reload()}>
+          <Text style={styles.reloadText}>Atualizar</Text>
+          <Feather name="refresh-cw" size={22} color="#f76981" />
+        </TouchableOpacity>
+        <Text style={styles.title}>Casos de Covid-19 no Brasil</Text>
+        <Text style={styles.description}>Comece escolhendo o estado</Text>
+
+        <FlatList
+          data={states}
+          style={styles.statesList}
+          keyExtractor={(state) => `${state.uid}`}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item: state }) => (
+            <View style={styles.states}>
+              <Text style={styles.stateProperty}>
+                {state.state}/{state.uf}
+              </Text>
+
+              <Text style={styles.stateProperty}>Casos:</Text>
+              <Text style={styles.stateValue}>{state.cases}</Text>
+
+              <TouchableOpacity
+                style={styles.detailsButton}
+                onPress={() => navigateToDetail(state)}
+              >
+                <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                <Feather name="arrow-right" size={16} color="#f76981" />
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+      </View>
+    );
+  }
 }
--- mobile/src/pages/Home/styles.js
+++ mobile/src/pages/Home/styles.js
@@ -1,92 +1,89 @@
-import {StyleSheet} from 'react-native';
-import Constants from 'expo-constants';
+import { StyleSheet } from "react-native";
+import Constants from "expo-constants";
 
 export default StyleSheet.create({
-    containerLoading:{
-        flex:1,
-        justifyContent:"center",
-        alignItems:"center",
-    },
-    container: {
-        flex: 1,
-        paddingHorizontal: 24,
-        paddingTop: Constants.statusBarHeight + 20,
-        backgroundColor:"#fff",
-    },
+  containerLoading: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20,
+    backgroundColor: "#fff",
+  },
 
-    header:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems:"center",
-    },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
 
-    headerText: {
-        fontSize: 15,
-        color: '#737380',
-    },
-    headerTextBold: {
-        fontWeight: 'bold',
-    },
+  headerText: {
+    fontSize: 15,
+    color: "#737380",
+  },
+  headerTextBold: {
+    fontWeight: "bold",
+  },
 
-    title:{
-        fontSize: 30,
-        marginBottom: 16,
-        marginTop: 15,
-        color: '#13131a',
-        fontWeight: 'bold',
-    },
+  title: {
+    fontSize: 30,
+    marginBottom: 16,
+    marginTop: 15,
+    color: "#13131a",
+    fontWeight: "bold",
+  },
 
-     description: {
-        fontSize: 18,
-        lineHeight: 24,
-        color: '#737380',
-        marginBottom: 32,
-    },
+  description: {
+    fontSize: 18,
+    lineHeight: 24,
+    color: "#737380",
+    marginBottom: 32,
+  },
+  states: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: "#f9f9f9",
+    marginBottom: 16,
+  },
 
-    stateList: {
-    },
+  stateProperty: {
+    fontSize: 14,
+    color: "#41414d",
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
 
-    states: {
-        padding: 24,
-        borderRadius: 8,
-        backgroundColor: '#f9f9f9',
-        marginBottom: 16,
-    },
+  stateValue: {
+    marginTop: 0,
+    fontSize: 15,
+    marginBottom: 24,
+    color: "#737380",
+  },
 
-    stateProperty:{
-        fontSize:14,
-        color: '#41414d',
-        fontWeight: 'bold',
-        marginBottom:20,
-    },
+  detailsButton: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
 
-    stateValue:{
-        marginTop: 0,
-        fontSize: 15,
-        marginBottom: 24,
-        color: '#737380',
-    },
-
-    detailsButton: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-
-    detailsButtonText: {
-        color: '#f76981',
-        fontSize: 15,
-        fontWeight: 'bold',
-    },
-    reload:{
-        marginTop:15,
-        flexDirection: 'row',
-        justifyContent:'flex-end'
-    },
-    reloadText:{
-        marginRight: 10,
-        fontWeight:"bold",
-        fontSize: 16,
-        color: '#f76981'
-    }
+  detailsButtonText: {
+    color: "#f76981",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  reload: {
+    marginTop: 15,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  reloadText: {
+    marginRight: 10,
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#f76981",
+  },
 });
